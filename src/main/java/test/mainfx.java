1dff9fa346cf4afed94e011d60a86a7b9d732261
ElaNourNeily <elaenour.neily@esprit.tn>
2026-05-05 16:08:47 +0100

    Save local changes before merge

--- src/main/java/test/mainfx.java
+++ src/main/java/test/mainfx.java
@@ -5,36 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.user.User;
-import service.user.UserService;
-import utils.RememberMe;
-import utils.Session;
 
 public class MainFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("LifeOps");
 
-        // Check for a saved "remember me" session
-        String savedEmail = RememberMe.load();
-        if (savedEmail != null) {
-            UserService userService = new UserService();
-            User user = userService.getUserByEmail(savedEmail);
-            if (user != null) {
-                Session.getInstance().setCurrentUser(user);
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/user/MainLayout.fxml"));
-                Parent root = loader.load();
-                stage.setScene(new Scene(root));
-                stage.show();
-                return;
-            }
-        }
-
-        // No saved session → show login
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/user/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Task/login.fxml"));
         Parent root = loader.load();
-        stage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        stage.setTitle("LifeOps - Login");
+        stage.setScene(scene);
         stage.show();
     }
 
